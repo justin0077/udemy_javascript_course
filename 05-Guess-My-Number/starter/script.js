9e0658bfc29152f0511ebc3d7d000e2b30f143fe
Justin <>
2022-08-26 15:36:41 +0200

    Start section 7

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+/*
+document.querySelector('.message').textContent = 'Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 15;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+/* Normal Declaration */
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+// });
+
+/* Arrow Function */
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // When no input
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess to high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+    // When guess to low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = twenty;
+});
